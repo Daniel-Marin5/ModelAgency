ef91cf7d75fc6b1036dbd47df9b0387a26b950a2
Daniel-Marin5 <X00215334@mytudublin.ie>
2025-05-01 15:02:38 +0100

    sign out bug

--- search_app/views.py
+++ search_app/views.py
@@ -1,6 +1,5 @@
 from sobaka.models import Human
 from django.views.generic import ListView
-from django.db.models import Q
 
 class SearchResultsListView(ListView):
     model = Human
@@ -10,18 +9,29 @@ class SearchResultsListView(ListView):
     def get_queryset(self):
         query = self.request.GET.get('q')
         attribute = self.request.GET.get('attribute', 'name')
-        if query:
+
+        valid_attributes = [
+            'name', 'height', 'bust_size', 'hip_size',
+            'waist_size', 'shoe_size', 'eye_color', 'hair_color'
+        ]
+
+        if not query or attribute not in valid_attributes:
+            return Human.objects.none()
+
+        if attribute in ['height', 'bust_size', 'hip_size', 'waist_size', 'shoe_size']:
+            filter_kwargs = {f"{attribute}__exact": query}
+        else:
             filter_kwargs = {f"{attribute}__icontains": query}
-            return Human.objects.filter(**filter_kwargs)
-        return Human.objects.all()
+
+        return Human.objects.filter(**filter_kwargs)
 
     def get_context_data(self, **kwargs):
         context = super(SearchResultsListView, self).get_context_data(**kwargs)
         query = self.request.GET.get('q')
         attribute = self.request.GET.get('attribute', 'name')
+
         attribute_labels = {
             'name': 'Name',
-            'category__name': 'Category',
             'height': 'Height',
             'bust_size': 'Bust Size',
             'hip_size': 'Hip Size',
@@ -30,6 +40,7 @@ class SearchResultsListView(ListView):
             'eye_color': 'Eye Color',
             'hair_color': 'Hair Color',
         }
+
         context['query'] = query
         context['attribute'] = attribute
         context['attribute_label'] = attribute_labels.get(attribute, attribute.title())
